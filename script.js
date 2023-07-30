ddef3741988d0deb32c7945e0c0a68250d71407b
Jesse Xia <68565085+x-jesse@users.noreply.github.com>
2023-07-30 02:37:18 -0400

    update theme

---
+++ script.js
@@ -0,0 +1,11 @@
+
+function show () {
+    console.log(document.getElementById('arrow').classList)
+    document.getElementById('arrow').style.opacity = 1;
+    document.getElementById('paste_links').style.opacity = 1;
+}
+
+function hide() {
+    document.getElementById('arrow').style.opacity = 0;
+    document.getElementById('paste_links').style.opacity = 0;
+}
