4b2b835d59b04be621ed2b0978ee6f5fe6d1f52a
Jesse Xia <68565085+x-jesse@users.noreply.github.com>
2023-10-03 15:55:52 -0400

    Add player and foreground anim

--- script.js
+++
@@ -1,9 +0,0 @@
-function show () {
-    document.getElementById('arrow').style.opacity = 1;
-    document.getElementById('paste_links').style.opacity = 1;
-}
-
-function hide() {
-    document.getElementById('arrow').style.opacity = 0;
-    document.getElementById('paste_links').style.opacity = 0;
-}
